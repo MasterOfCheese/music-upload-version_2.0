463714e67b5137b0ec9a632fda3d9d177f1563b6
Nguyen Phuong Nam <140500867+MasterOfCheese@users.noreply.github.com>
2025-06-29 08:46:59 +0700

    Fix Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  base: './'
 })
